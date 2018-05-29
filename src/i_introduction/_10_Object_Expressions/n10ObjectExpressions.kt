764f2ef958177426f4d041cb9a92274a051134b1
Valiantsin Pshanichnik <valiktorus@mail.ru>
2018-05-30 01:30:30 +0300

    task introduction 10 lambda changed to object

--- src/i_introduction/_10_Object_Expressions/n10ObjectExpressions.kt
+++ src/i_introduction/_10_Object_Expressions/n10ObjectExpressions.kt
@@ -3,6 +3,7 @@ package i_introduction._10_Object_Expressions
 import util.TODO
 import util.doc10
 import java.util.*
+import kotlin.Comparator
 
 fun todoTask10(): Nothing = TODO(
     """
@@ -18,6 +19,13 @@ fun todoTask10(): Nothing = TODO(
 
 fun task10(): List<Int> {
     val arrayList = arrayListOf(1, 5, 2)
-    Collections.sort(arrayList, { o1, o2 -> o2 - o1 })
+    Collections.sort(
+            arrayList,
+            object : Comparator<Int>{
+                override fun compare(o1: Int, o2: Int): Int {
+                    return o2 - o1
+                }
+            }
+    )
     return arrayList
 }
