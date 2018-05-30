4fa28233b8a75c4a213a8cfece6797863c5ddf9f
Valiantsin Pshanichnik <valiktorus@mail.ru>
2018-05-30 22:30:32 +0300

    task collections 20

--- src/ii_collections/n20GroupBy.kt
+++ src/ii_collections/n20GroupBy.kt
@@ -6,7 +6,5 @@ fun example7() {
     result == mapOf(1 to listOf("a", "b"), 2 to listOf("ba", "ad"), 3 to listOf("ccc"))
 }
 
-fun Shop.groupCustomersByCity(): Map<City, List<Customer>> {
+fun Shop.groupCustomersByCity(): Map<City, List<Customer>> = customers.groupBy(Customer::city)
     // Return a map of the customers living in each city
-    todoCollectionTask()
-}
