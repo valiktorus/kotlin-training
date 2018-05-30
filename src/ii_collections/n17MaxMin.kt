d5fdf5bdecbe190d40687ea3349daabad1ce5f88
Valiantsin Pshanichnik <valiktorus@mail.ru>
2018-05-30 22:19:05 +0300

    task collections 17

--- src/ii_collections/n17MaxMin.kt
+++ src/ii_collections/n17MaxMin.kt
@@ -5,12 +5,8 @@ fun example4() {
     val longestString = listOf("a", "b").maxBy { it.length }
 }
 
-fun Shop.getCustomerWithMaximumNumberOfOrders(): Customer? {
+fun Shop.getCustomerWithMaximumNumberOfOrders(): Customer?  = customers.maxBy { it.orders.size }
     // Return a customer whose order count is the highest among all customers
-    todoCollectionTask()
-}
 
-fun Customer.getMostExpensiveOrderedProduct(): Product? {
+fun Customer.getMostExpensiveOrderedProduct(): Product? = orders.flatMap(Order::products).maxBy(Product::price)
     // Return the most expensive product which has been ordered
-    todoCollectionTask()
-}
