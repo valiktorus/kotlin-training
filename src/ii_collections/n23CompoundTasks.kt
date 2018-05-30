3db01134015d38b056b83f7889e4fcffc96ee51c
Valiantsin Pshanichnik <valiktorus@mail.ru>
2018-05-31 00:20:14 +0300

    task collections 23

--- src/ii_collections/n23CompoundTasks.kt
+++ src/ii_collections/n23CompoundTasks.kt
@@ -1,18 +1,23 @@
 package ii_collections
 
-fun Shop.getCustomersWhoOrderedProduct(product: Product): Set<Customer> {
-    // Return the set of customers who ordered the specified product
-    todoCollectionTask()
-}
+fun Shop.getCustomersWhoOrderedProduct(product: Product): Set<Customer> =
+        customers.filter { it.orders.flatMap(Order::products).contains(product) }
+                .toSet()
 
-fun Customer.getMostExpensiveDeliveredProduct(): Product? {
+    // Return the set of customers who ordered the specified product
+
+
+fun Customer.getMostExpensiveDeliveredProduct(): Product? =
+        orders.filter(Order::isDelivered)
+                .flatMap(Order::products)
+                .maxBy(Product::price)
     // Return the most expensive product among all delivered products
     // (use the Order.isDelivered flag)
-    todoCollectionTask()
-}
 
-fun Shop.getNumberOfTimesProductWasOrdered(product: Product): Int {
+
+fun Shop.getNumberOfTimesProductWasOrdered(product: Product): Int =
+        customers.flatMap(Customer::orders)
+                .flatMap(Order::products)
+                .count{ it == product }
     // Return the number of times the given product was ordered.
     // Note: a customer may order the same product for several times.
-    todoCollectionTask()
-}
