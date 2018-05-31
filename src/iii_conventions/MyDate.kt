d0d56df78b880a5b63035aba7be7eb243512af79
Valiantsin Pshanichnik <valiktorus@mail.ru>
2018-06-01 00:10:56 +0300

    task convention 29

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -13,11 +13,19 @@ data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparab
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
 
+operator fun MyDate.plus(interval: TimeInterval): MyDate = addTimeIntervals(interval, 1)
+
+operator fun MyDate.plus(multiInterval: MultiInterval): MyDate = addTimeIntervals(multiInterval.interval, multiInterval.number)
+
+data class MultiInterval(val interval: TimeInterval, val number: Int)
+
 enum class TimeInterval {
     DAY,
     WEEK,
     YEAR
 }
+operator fun TimeInterval.times(number: Int): MultiInterval = MultiInterval(this, number)
+
 
 class DateRange(override val start: MyDate, override val endInclusive: MyDate) : ClosedRange<MyDate>, Iterable<MyDate> {
     override fun iterator(): Iterator<MyDate> {
