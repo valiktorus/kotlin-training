d52d274915683616aea87cce09ebe7061162f95b
Valiantsin Pshanichnik <valiktorus@mail.ru>
2018-06-01 00:36:48 +0300

    task convention 31

--- src/iii_conventions/n31Invoke.kt
+++ src/iii_conventions/n31Invoke.kt
@@ -3,7 +3,14 @@ package iii_conventions
 import util.TODO
 
 
-class Invokable
+class Invokable(){
+    var invokeNumber = 0
+    operator fun invoke(): Invokable  {
+        invokeNumber++
+        return this
+    }
+    fun getNumberOfInvocations() = invokeNumber
+}
 
 fun todoTask31(): Nothing = TODO(
     """
@@ -14,6 +21,5 @@ fun todoTask31(): Nothing = TODO(
     references = { invokable: Invokable -> })
 
 fun task31(invokable: Invokable): Int {
-    todoTask31()
-//    return invokable()()()().getNumberOfInvocations()
+    return invokable()()()().getNumberOfInvocations()
 }
