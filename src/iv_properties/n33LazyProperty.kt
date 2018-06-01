7208267c8350f978380447e2100ed446b0d429b2
Valiantsin Pshanichnik <valiktorus@mail.ru>
2018-06-01 15:22:41 +0300

    task properties 33

--- src/iv_properties/n33LazyProperty.kt
+++ src/iv_properties/n33LazyProperty.kt
@@ -3,7 +3,11 @@ package iv_properties
 import util.TODO
 
 class LazyProperty(val initializer: () -> Int) {
-    val lazy: Int = todoTask33()
+    val lazy: Int = 0
+    get(){
+        if (field == 0) field = initializer()
+        return field
+    }
 }
 
 fun todoTask33(): Nothing = TODO(
